977bc7b7eb54461db2ddb8e9193f05c7880e6be2
Alex Murray <murray.alex@gmail.com>
2022-01-18 12:57:31 +1030

    Fix xdg-open for Ubuntu 22.04 and cleanup site-start.el
    Emacs needs to set it's own GIO_MODULE_DIR so that it uses the gio modules
    which come from it's base snap but the processes launched by Emacs come
    from the host environment and in this case they should *not* use the gio
    modules from the Emacs snap so make sure we unset GIO_MODULE_DIR in
    site-start.el
    
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- site-lisp/site-start.el
+++ site-lisp/site-start.el
@@ -1,5 +1,27 @@
-;; we don't want subprocesses inheriting the snap specific GDK_PIXBUF and
-;; FONTCONFIG environment so make sure they are effective unset
+;;; site-start.el --- Make Emacs work correctly as a snap
+
+;;; Commentary:
+
+;; This file contains the various settings to allow Emacs to function
+;; correctly as a strictly confined snap
+
+;;; Code:
+
+;; since the Emacs snap is under classic confinement, it runs in the host
+;; systems mount namespace and hence will use the host system's PATH
+;; etc. As such, we don't want subprocesses launched by Emacs to inherit
+;; the snap specific GIO_MODULE_DIR, GDK_PIXBUF and FONTCONFIG environment
+;; as they likely will be linked against different libraries than what the
+;; Emacs snap base snap is using. So make sure they are effectively unset.
+(setenv "GIO_MODULE_DIR")
 (setenv "GDK_PIXBUF_MODULE_FILE")
 (setenv "GDK_PIXBUF_MODULEDIR")
 (setenv "FONTCONFIG_FILE")
+
+;; ensure native-comp can find the compiler
+(eval-when-compile
+  (require 'comp))
+(setq native-comp-driver-options '("--sysroot=/snap/emacs/current/" "-B/snap/emacs/current/usr/lib/gcc/"))
+
+(provide 'site-start)
+;;; site-start.el ends here
